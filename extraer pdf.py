9c876ef8dcd84beae75bf07edd1cc1af56de8eb1
JJTF <josue.tapia@pucp.edu.pe>
2022-02-07 12:58:19 -0500

    primera versión

---
+++ extraer pdf.py
@@ -0,0 +1,11 @@
+import fitz
+import os
+ruta="D:\Sanipes"
+os.chdir(ruta)
+pdf_doc="sanipes.pdf"
+doc=fitz.open(pdf_doc)
+print("nun pág:", doc.pageCount)
+print('metadatos:', doc.metadata)
+pagina=doc.loadPage(0)
+text=pagina.getText("text")
+print(text)
